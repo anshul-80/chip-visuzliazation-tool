9c3ac0a8c6b8984fd58a3e12a0702311aa6686e4
asmita kovadia <122500760+anshul-80@users.noreply.github.com>
2023-04-18 23:25:10 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,81 @@
-import configparser
 import mysql.connector
+import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Access the values using the section and option names
-db_hostname = config.get('db', 'host')
-db_username = config.get('db', 'user')
-db_password = config.get('db', 'password')
-db_database = config.get('db', 'database')
+db_config = config['db']
 
-# Print the values
-print(f"Database Hostname: {db_hostname}")
-print(f"Database Username: {db_username}")
-print(f"Database Password: {db_password}")
-print(f"Database Name: {db_database}")
-
-# Connect to the database
 cnx = mysql.connector.connect(
-    host=db_hostname,
-    database=db_database,
-    user=db_username,
-    password=db_password
+    user=db_config['user'],
+    password=db_config['password'],
+    host=db_config['host'],
+    database=db_config['database']
 )
 
+# Use the connection to interact with the database
+cursor = cnx.cursor()
+query = "SELECT * FROM users"
+cursor.execute(query)
+results = cursor.fetchall()
+cursor.close()
+cnx.close()
+
+import mysql.connector
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db_config = config['db']
+
+# Connect to the MySQL database
+cnx = mysql.connector.connect(
+    user=db_config['user'],
+    password=db_config['password'],
+    host=db_config['host'],
+    database=db_config['database']
+)
+
+# Create a cursor object to execute SQL queries
 c = cnx.cursor()
 
+# Define functions for CRUD operations
 def create_user(first_name, last_name, contact_number ):
     """Create a new user."""
-    c.execute("INSERT INTO users (first_name, last_name, contact_number ) VALUES ( ?, ?, ?)", ( first_name, last_name, contact_number, ))
+    c.execute("INSERT INTO users (first_name, last_name, contact_number ) VALUES ( %s, %s, %s)", ( first_name, last_name, contact_number, ))
     cnx.commit()
     print("User created successfully.")
 
 def read_user(id):
     """Read user details by ID."""
-
-    # Define a query with placeholders
-   # query = "SELECT * FROM users WHERE id = %s"
-
-    # Provide values for the placeholders
-    #params = (tuple(id))
-
-    #c.execute(query, params)
-
-    c.execute("SELECT * FROM users WHERE id=?", (id))
+    sql = "SELECT * FROM users WHERE id=%s"
+    c.execute(sql, (id,))
     user = c.fetchone()
     if user:
         print(f"ID: {user[0]}, first_name: {user[1]}, last_name: {user[2]}, contact_number: {user[5]}")
     else:
         print("User not found.")
 
+def update_user(user_id, first_name, last_name):
+    """Update user details by ID."""
+    c.execute("UPDATE users SET first_name=%s, last_name=%s WHERE id=%s", (first_name, last_name, user_id))
+    cnx.commit()
+    print("User updated successfully.")
 
-# CRUD operations
+def delete_user(user_id):
+    """Delete user by ID."""
+    c.execute("DELETE FROM users WHERE id=%s", (user_id,))
+    cnx.commit()
+    print("User deleted successfully.")
+
+# Main program loop for CRUD operations
 while True:
     print("1. Create User")
     print("2. Read User")
-    print("6. Exit")
+    print("3. Update User")
+    print("4. Delete User")
+    print("5. Exit")
     choice = input("Enter your choice: ")
 
     if choice == '1':
@@ -68,10 +88,24 @@ while True:
         user_id = input("Enter user ID: ")
         read_user(user_id)
 
-    elif choice == '6':
+    elif choice == '3':
+        user_id = input("Enter user ID: ")
+        first_name = input("Enter first_name: ")
+        last_name = input("Enter last_name: ")
+        update_user(user_id, first_name, last_name)
+
+    elif choice == '4':
+        user_id = input("Enter user ID: ")
+        delete_user(user_id)
+
+    elif choice == '5':
         print("Exiting...")
         break
 
     else:
         print("Invalid choice. Please try again.")
+
+# Close the cursor and connection
+c.close()
+cnx.close()
 
